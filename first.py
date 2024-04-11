793a64afbd66e06ddeae97e120e5071a8022f5e7
Jigneshkotia <140388708+Jigneshkotia@users.noreply.github.com>
2024-04-11 20:19:53 +0000

    first commit

---
+++ first.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+st.set_page_config(page_title="ML Data Visualiser ",
+                   layout="centered",
+                   page_icon="📊")
+
+st.title("📊 Data visualiser")
